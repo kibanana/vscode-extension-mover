4d8b2247e017515924deca0e0112f3960f3a17cc
Kim Yewon <kyw017763@gmail.com>
2021-09-25 23:51:18 +0900

    Remove extension.deletingHelper command

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import open from 'open';
 import * as os from 'os';
 import osName from 'os-name';
-import osObj, { osType } from './modules/osType';
+import { osObj, osType } from './modules/osType';
 import fsSync from './modules/fsSync';
 import MESSAGE from './modules/messages';
 import getExtensionList from './modules/getExtensionList';
@@ -10,84 +10,97 @@ import saveExtensionListAsFile from './modules/saveExtensionListAsFile';
 import copyExtensionList from './modules/copyExtensionList';
 
 export function activate(context: vscode.ExtensionContext) {
-  const exporter = vscode.commands.registerCommand('extension.exporter', async () => {
-    const osOption: string = osName();
+    const exporter = vscode.commands.registerCommand('extension.exporter', async () => {
+        const executeExportHelper = async () => {
+            const osOption: string = osName();
 
-    let extensions: string | null | undefined = await getExtensionList({ osOption });
-    if (!extensions || typeof extensions !== 'string') {
-      return vscode.window.showErrorMessage(MESSAGE.EXPORTATION_EXECUTION_ABORTED);
-    }
+            let extensions: string | null | undefined = await getExtensionList({
+                osOption
+            });
+            if (!extensions || typeof extensions !== 'string') {
+                return vscode.window.showErrorMessage(MESSAGE.EXPORTATION_EXECUTION_ABORTED);
+            }
 
-    let extensionList: string[] = extensions.split('\n');
-    extensionList = extensionList.slice(0, extensionList.length - 1);
-    const extensionCnt = extensionList.length;
+            let extensionList: string[] = extensions.split('\n');
+            extensionList = extensionList.slice(0, extensionList.length - 1);
+            const extensionCnt = extensionList.length;
 
-    const commandList: string[] = [];
-    extensionList.forEach((elem: string) => commandList.push(`code --install-extension ${elem}${os.EOL}`));
+            const commandList: string[] = [];
+            extensionList.forEach((elem: string) => commandList.push(`code --install-extension ${elem}${os.EOL}`));
 
-    const commandOptions: string[] = ['Save as File', 'Save to Clipboard'];
-    const commandOption = await vscode.window.showQuickPick(commandOptions, {
-      placeHolder: 'Select where to save the command',
-      ignoreFocusOut: true
+            const commandOptions: string[] = ['Save as File', 'Save to Clipboard'];
+            const commandOption = await vscode.window.showQuickPick(commandOptions, {
+                placeHolder: 'Select where to save the command',
+                ignoreFocusOut: true
+            });
+
+            if (!commandOption) {
+                return vscode.window.showErrorMessage(MESSAGE.INVALID_VALUE);
+            }
+
+            let exportResult: boolean | undefined = false;
+            let exportStatus: string = '';
+            if (commandOption === commandOptions[0]) {
+                exportResult = await saveExtensionListAsFile({
+                    commands: commandList.join('')
+                });
+                exportStatus = 'saved';
+            } else if (commandOption === commandOptions[1]) {
+                exportResult = await copyExtensionList({
+                    osOption,
+                    commands: commandList.join('')
+                });
+                exportStatus = 'copied';
+            }
+
+            if (exportResult) {
+                return vscode.window.showInformationMessage(MESSAGE.EXPORTATION_EXECUTION_RESULT(extensionCnt, exportStatus));
+            }
+
+            vscode.window.showErrorMessage(MESSAGE.EXPORTATION_EXECUTION_ABORTED);
+
+            context.subscriptions.push(exporter);
+        };
+
+        const executeDeleteHeloper = async () => {
+            const deletingHelpOptions: string[] = ["Yes", "No"];
+            const result: string | undefined = await vscode.window.showInformationMessage('Would you like to delete all the vscode extensions?', ...deletingHelpOptions);
+            if (result !== deletingHelpOptions[0]) {
+                return vscode.window.showInformationMessage(MESSAGE.HELPING_DELETION_EXECUTION_ABORTED);
+            }
+
+            const osOption: string = osName();
+            let defaultExtensionPath: string = '';
+            if (osOption.includes(osType[2])) {
+                defaultExtensionPath = osObj[osType[2]];
+            } else if (osOption.includes(osType[0])) {
+                defaultExtensionPath = osObj[osType[0]];
+            }
+
+            const extensionPath = await vscode.window.showInputBox({
+                value: defaultExtensionPath,
+                placeHolder: `Enter the vscode extension path (default: ${defaultExtensionPath})`,
+                ignoreFocusOut: true
+            });
+            if (!extensionPath) {
+                return vscode.window.showErrorMessage(MESSAGE.INVALID_VALUE);
+            }
+
+            const isExists = await fsSync.AccessFileSync(extensionPath);
+            if (!isExists) {
+                return vscode.window.showErrorMessage(MESSAGE.IS_NOT_EXISTS);
+            }
+
+            vscode.window.showInformationMessage('Please delete the directory yourself for accurate operation!');
+
+            setTimeout(() => {
+                open(extensionPath);
+            }, 1000);
+        };
+
+        await executeExportHelper();
+        await executeDeleteHeloper();
     });
-
-    if (!commandOption) {
-      return vscode.window.showErrorMessage(MESSAGE.INVALID_VALUE);
-    }
-    
-    let exportResult: boolean | undefined = false;
-    let exportStatus: string = '';
-    if (commandOption === commandOptions[0]) {
-      exportResult = await saveExtensionListAsFile({ commands: commandList.join('') });
-      exportStatus = 'saved';
-    } else if (commandOption === commandOptions[1]) {
-      exportResult = await copyExtensionList({ osOption, commands: commandList.join('') });
-      exportStatus = 'copied';
-    }
-
-    if (exportResult) {
-      return vscode.window.showInformationMessage(MESSAGE.EXPORTATION_EXECUTION_RESULT(extensionCnt, exportStatus));
-    }
-
-    vscode.window.showErrorMessage(MESSAGE.EXPORTATION_EXECUTION_ABORTED);
-  });
-
-  const deletingHelper = vscode.commands.registerCommand('extension.deletingHelper', async () => {
-    const deletingHelpOptions: string[] = ["Yes", "No"];
-    const result: string | undefined = await vscode.window.showInformationMessage('Would you like to delete all the vscode extensions?', ...deletingHelpOptions);
-    if (result !== deletingHelpOptions[0]) {
-      return vscode.window.showInformationMessage(MESSAGE.HELPING_DELETION_EXECUTION_ABORTED);
-    }
-
-    const osOption: string = osName();
-    let defaultExtensionPath: string = '';
-    if (osOption.includes(osType[2])) {
-      defaultExtensionPath = osObj[osType[2]];
-    } else if (osOption.includes(osType[0])) {
-      defaultExtensionPath = osObj[osType[0]];
-    }
-
-    const extensionPath = await vscode.window.showInputBox({
-      value: defaultExtensionPath,
-      placeHolder: `Enter the vscode extension path (default: ${defaultExtensionPath})`,
-      ignoreFocusOut: true
-    });
-    if (!extensionPath) {
-      return vscode.window.showErrorMessage(MESSAGE.INVALID_VALUE);
-    }
-
-    const isExists = await fsSync.AccessFileSync(extensionPath);
-    if (!isExists) {
-      return vscode.window.showErrorMessage(MESSAGE.IS_NOT_EXISTS);
-    }
-
-    vscode.window.showInformationMessage('Please delete the directory yourself for accurate operation!');
-
-    setTimeout(() => { open(extensionPath); }, 1000);
-  });
-
-  context.subscriptions.push(exporter);
-  context.subscriptions.push(deletingHelper);
 }
 
 export function deactivate() {}
--- src/modules/osType.ts
+++ src/modules/osType.ts
@@ -2,11 +2,10 @@ import * as path from 'path';
 import os from 'os';
 import OsType from '../ts/OsType';
 
-const osObj: OsType = {
+export const osObj: OsType = {
   macOS: path.resolve(os.homedir(), '.vscode', 'extensions'),
   Linux: path.resolve(os.homedir(), '.vscode', 'extensions'),
   Windows: path.resolve(os.homedir(), '.vscode', 'extensions')
 };
 
-export default osObj;
 export const osType = Object.keys(osObj);
